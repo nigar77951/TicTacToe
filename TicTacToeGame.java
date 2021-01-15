57fb0dfeef405b2882ec107a53b287ca06707a38
unknown <nigar77951@gmail.com>
2021-01-16 00:44:17 +0530

    user choice 'o' or 'x'

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,4 +1,7 @@
+import java.util.*;
 public class TicTacToeGame {
+
+	public String Player, Computer;
 
     public void boardCreation() {
         char[] board= new char[9];
@@ -8,11 +11,30 @@ public class TicTacToeGame {
         }
     }
 
+    public void xorO(char choice) {
+
+    	if(choice == 'x') {
+    		Player="o";
+    	}
+    	else if(choice == 'o') {
+    		Computer = "x";
+    	}
+    	else {
+    		System.out.println("not a valid choice");
+    	}
+
+    }
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
         System.out.println("...wlcm to tic tac toe game...");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the choice 'o' or 'x'");
+        char choice = sc.next().charAt(0);
+
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.boardCreation();
-
+        ticTacToeGame.xorO(choice);
 	}
-
 }
