955e1ebd956de81ec961f92d33185884969b9829
unknown <nigar77951@gmail.com>
2021-01-15 23:54:14 +0530

    TTT board creation

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,18 @@
 public class TicTacToeGame {
+
+    public void boardCreation() {
+        char[] board= new char[9];
+        for(int i=1;i<9;i++)
+        {
+            System.out.println(" "+board[i]);
+        }
+    }
+
 	public static void main(String[] args) {
-		System.out.println("...wlcm to tic tac toe game...");
+        System.out.println("...wlcm to tic tac toe game...");
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.boardCreation();
+
 	}
+
 }
